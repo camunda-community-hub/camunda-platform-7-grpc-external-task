d20eb565a88bf487984dbeefde9c092136f3e104
Tobias Metzke <tobias.metzke@camunda.com>
2019-08-14 11:45:05 +0200

    start process instances on startup

--- src/main/java/org/camunda/bpm/grpc/CamundaApplication.java
+++ src/main/java/org/camunda/bpm/grpc/CamundaApplication.java
@@ -1,13 +1,29 @@
 package org.camunda.bpm.grpc;
 
+import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableProcessApplication("camunda.bpm.grpc.external.task.server")
-public class CamundaApplication {
+public class CamundaApplication  {
+
+  @Autowired
+  private ProcessEngine processEngine;
+
   public static void main(String... args) {
     SpringApplication.run(CamundaApplication.class, args);
   }
+
+  @EventListener
+  public void onPostDeploy(PostDeployEvent event) {
+    processEngine.getRuntimeService().startProcessInstanceByKey("camunda.bpm.grpc.external.task.server");
+    processEngine.getRuntimeService().startProcessInstanceByKey("camunda.bpm.grpc.external.task.server");
+    processEngine.getRuntimeService().startProcessInstanceByKey("camunda.bpm.grpc.external.task.server");
+  }
+
 }
